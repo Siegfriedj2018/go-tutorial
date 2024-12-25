2bf0bac16ec074f83bc58dc8bda9bd9a1fc5222a
Siegfriedj2018 <siegfrij@email.sc.edu>
2024-12-25 12:02:40 -0500

    Got go to connect to a database

---
+++ databases/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"fmt"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+// database handle--Dont do this in production
+var db *sql.DB
+
+type Album struct {
+	ID		 int64
+	Title  string
+	Artist string
+	Price  float32
+}
+
+func main() {
+	// Capture connection properties
+	cfg := mysql.Config{
+		User:		"root",
+		Passwd: "$h@d0wF1r3Pr0",
+		Net:		"tcp",
+		Addr: 	"127.0.0.1:3306",
+		DBName: "recordings",
+	}
+
+	// Get a database handle.
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+
+	fmt.Println("Connected!")
+}
