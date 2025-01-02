388ee0f04d9cd20d691e0300b77bdbb12211595f
Siegfriedj2018 <siegfrij@email.sc.edu>
2025-01-02 16:12:31 -0500

    channels in go...

--- concurrency/channels.go
+++ concurrency/channels.go
@@ -10,6 +10,12 @@ func sum (s []int, c chan int) {
 	c <- sum // send sum to c
 }
 
-func main () {
-	
+func main() {
+	s := []int{7, 2, 8, -9, 4, 0}
+
+	c := make(chan int)  // creates the channel
+	go sum(s[:len(s)/2], c) // sums the first half
+	go sum(s[len(s)/2:], c) // sums the second half
+	x, y := <-c, <-c // receive from c (like join?)
+	fmt.Println(x, y, x+y)
 }
