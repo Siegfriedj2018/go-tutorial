837616272a7f786a93aefb434fa025539584e34b
Siegfriedj2018 <siegfrij@email.sc.edu>
2025-01-21 15:42:09 -0500

    Finished web programming tutorial

--- codecademy_tutorials/web_programming/main.go
+++ codecademy_tutorials/web_programming/main.go
@@ -11,7 +11,7 @@ func main() {
 	mux.HandleFunc("/", handleHome)
 	mux.HandleFunc("GET /menu", handleMenu)
 	mux.HandleFunc("GET /reviews", handleReviews)
-	mux.HandleFunc("Get /review", handleReviewForm)
+	mux.HandleFunc("GET /review", handleReviewForm)
 	mux.HandleFunc("POST /submitReview", handleReviewSubmission)
 	// serve static files
 	fileServer := http.FileServer(http.Dir("./static"))
