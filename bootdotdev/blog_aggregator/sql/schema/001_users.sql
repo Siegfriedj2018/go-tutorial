0df32ecabc650f7b667e7d7b6435f2713ab96bb8
Siegfriedj2018 <siegfrij@email.sc.edu>
2025-10-06 04:56:14 -0400

    working on adding feeds to database

--- bootdotdev/blog_aggregator/sql/schema/001_users.sql
+++ bootdotdev/blog_aggregator/sql/schema/001_users.sql
@@ -6,5 +6,18 @@ CREATE TABLE users (
     name TEXT UNIQUE NOT NULL
 );
 
+CREATE TABLE feeds (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    name TEXT,
+    url TEXT UNIQUE NOT NULL,
+    user_id UUID,
+    CONSTRAINT fk_user_id
+        FOREIGN KEY(user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE
+);
+
 -- +goose Down
 DROP TABLE users;
